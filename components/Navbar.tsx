b1694456353fbd09ed8cfbbba49b46fd6c1d287f
algosourceio <algo.source.io@gmail.com>
2026-02-07 14:15:55 +0530

    ficup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -254,7 +254,7 @@ export default function Navbar() {
 
         {/* Proposals Link */}
         <Link
-          href="https://algosource.in/proposals"
+          href="https://algosource.in/proposal"
           className={`nav-link-btn flex items-center px-3 py-2 text-sm font-medium transition-colors ${
             isDark ? 'text-gray-400 hover:text-white' : 'text-gray-600 hover:text-gray-900'
           }`}
